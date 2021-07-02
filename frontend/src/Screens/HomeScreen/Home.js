429a1fb81639d0a9fd6a554d543bda29553b185d
PhilipNirushan <PhilipNirushan25@gmail.com>
2021-07-02 08:46:31 +0700

    Home Page added

---
+++ frontend/src/Screens/HomeScreen/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImageSlider from '../../Components/ImageSlider'
+import AboutSection from './AboutSection'
+
+const Home = () => {
+    return (
+        <div>
+            <ImageSlider/>
+            <AboutSection/>
+        </div>
+    )
+}
+
+export default Home
