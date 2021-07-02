f0750256980f326e01cbc8ed333fd3163b8b3891
PhilipNirushan <PhilipNirushan25@gmail.com>
2021-07-02 08:35:38 +0700

    Nav bar updated

--- frontend/src/Components/Navbar/Header.js
+++ frontend/src/Components/Navbar/Header.js
@@ -1,28 +1,42 @@
 import React from 'react'
-import logo from '../Images/logo.png'
+import { LinkContainer } from 'react-router-bootstrap'
 import './Header.css'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
     return (
         <header >
-            <Navbar bg="myNav" variant="dark" expand="lg" sticky="top" collapseOnSelect>     
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} className="d-inline-block align-top imgLogo"  alt="Logo" fluid/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="#home" className="text-white"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-                        <Nav.Link href="#link" className="text-white"><i className="fas fa-user"></i> Sign In</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-
-            </Container>
-
-        </Navbar>
-    </header>
+            <Navbar bg="myNav" variant="dark" expand="lg" sticky="top" collapseOnSelect>
+                <Container fluid>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>
+                            <img src="/Images/logo.png" className="d-inline-block align-top imgLogo" alt="Logo" fluid />
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <LinkContainer to='/' exact>
+                                <Nav.Link className="text-white">Home</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/shops' exact>
+                                <Nav.Link className="text-white">Shops</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/contact'>
+                                <Nav.Link className="text-white">Contact</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link className="text-white"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link className="text-white"><i className="fas fa-user"></i> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
 
