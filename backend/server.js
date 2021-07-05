86bdf8e6cd20b3740dcd7fcdb4620ef951b25d97
PhilipNirushan <PhilipNirushan25@gmail.com>
2021-07-05 23:12:22 +0700

    Connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
-// const products = require('./data/products')
 import shops from './data/shops.js'
+import events from './data/events.js'
 
 dotenv.config()
 
@@ -14,18 +15,19 @@ app.get('/', (req,res) => {
     res.send('API is running')
 })
 
-// app.get('/api/products', (req,res) => {
-//     res.json(products)
-// })
+
+app.get('/api/events', (req,res) => {
+    res.json(events)
+})
 
 app.get('/api/shops', (req,res) => {
     res.json(shops)
 })
 
-// app.get('/api/products/:id', (req,res) => {
-//     const product = products.find((p) => p._id === req.params.id)
-//     res.json(product)
-// })
+app.get('/api/events/:id', (req,res) => {
+    const event = events.find((e) => e._id === req.params.id)
+    res.json(event)
+})
 
 app.get('/api/shops/:id', (req,res) => {
     const shop = shops.find((s) => s._id === req.params.id)
@@ -34,4 +36,4 @@ app.get('/api/shops/:id', (req,res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold))
