b3e30b3d7c81494a53b575903cfa70bb259e9f4a
PhilipNirushan <PhilipNirushan25@gmail.com>
2021-07-10 19:58:41 +0700

    User Login Screen Component created

--- frontend/src/Screens/LoginScreen.js
+++ frontend/src/Screens/LoginScreen.js
@@ -1,42 +1,94 @@
-import React from 'react'
-import { Container, Row, Col, Form, Button, Image} from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../actions/userActions'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import Message from '../Components/Message'
+import Loader from '../Components/Loader'
 
+const LoginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-const LoginScreen = () => {
-    return (
-        <div className="my-4 my-5">
-            <Container>
-                <Row className="no-gutters form-row">
-                    <Col md="5">
-                        <Image src="/Images/interface.png" alt="login" fluid/>
-                    </Col>
-                    <Col md="7" className="px-5 pt-5">
-                        <h4>Sign into your account</h4>
-                        <Form>
-                            <Form.Row>
-                                <Col lg="7">
-                                <Form.Control type="email" placeholder="Enter email"  className="my-3 p-4"/>
-                                </Col>
-                            </Form.Row>
-                            <Form.Row>
-                                <Col lg="7">
-                                    <Form.Control type="password" placeholder="Password" className="my-3 p-4"/>
-                                </Col>
-                            </Form.Row>
-                            <Form.Row className="mt-3">
-                                <Col lg="7">
-                                    <Button variant="primary" type="button" className="btn1">
-                                        Login
-                                    </Button>
-                                </Col>
-                            </Form.Row>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+  const dispatch = useDispatch()
 
-    )
+  const userLogin = useSelector(state => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = e => {
+    e.preventDefault()
+    dispatch(login(email, password))
+  }
+
+  return (
+    <div className='my-4 my-5'>
+      <Container>
+        <Row className='justify-content-md-center'>
+          {/* <Col md='5'>
+            <Image src='/Images/interface.png' alt='login' fluid />
+          </Col> */}
+          <Col xs={12} md={7} className='px-5 pt-5'>
+            <h4>Sign into your account</h4>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
+              <Form.Row>
+                <Col lg='7'>
+                  <Form.Group controlId='email'>
+                    <Form.Control
+                      type='email'
+                      placeholder='Enter email'
+                      value={email}
+                      onChange={e => setEmail(e.target.value)}
+                      className='my-3 p-4'
+                    />
+                  </Form.Group>
+                </Col>
+              </Form.Row>
+              <Form.Row>
+                <Col lg='7'>
+                  <Form.Group controlId='password'>
+                    <Form.Control
+                      type='password'
+                      placeholder='Password'
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                      className='my-3 p-4'
+                    />
+                  </Form.Group>
+                </Col>
+              </Form.Row>
+              <Form.Row className='mt-3'>
+                <Col lg='7'>
+                  <Button type='submit' variant='primary' className='btn1'>
+                    Sign In
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+            <Row className='py-3'>
+              <Col>
+                New Customer?{' '}
+                <Link
+                  to={redirect ? `/register?redirect=${redirect}` : '/register'}
+                >
+                  Register
+                </Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
 }
 
 export default LoginScreen
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { shopListReducer, shopDetailsReducer } from './reducers/shopReducer'
 import { eventListReducer, eventDetailsReducer } from './reducers/eventReducer'
 import { cartReducer } from './reducers/cartReducer'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
   shopList: shopListReducer,
@@ -11,14 +12,20 @@ const reducer = combineReducers({
   eventList: eventListReducer,
   eventDetails: eventDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 
 const cartItemsFormStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFormStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const initialState = {
   cart: { cartItems: cartItemsFormStorage },
+  userLogin: { userInfo: userInfoFormStorage },
 }
 
 const middleware = [thunk]
