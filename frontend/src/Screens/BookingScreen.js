a542ae7885aa2bcb29870c11fc5ca158ead2bf37
PhilipNirushan <PhilipNirushan25@gmail.com>
2021-07-15 14:09:18 +0700

    Adding PayPal Payments

--- frontend/src/Screens/BookingScreen.js
+++ frontend/src/Screens/BookingScreen.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from 'react'
-import {
-  Container,
-  Row,
-  Col,
-  ListGroup,
-  Image,
-  Card,
-  Button,
-} from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import { PayPalButton } from 'react-paypal-button-v2'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { getBookingDetails } from '../actions/bookingActions'
+import { getBookingDetails, payBooking } from '../actions/bookingActions'
+import { BOOKING_PAY_RESET } from '../constants/bookingConstants'
 import Message from '../Components/Message'
 import Loader from '../Components/Loader'
+import axios from 'axios'
 
 const BookingScreen = ({ match }) => {
   const bookingId = match.params.id
+
+  const [sdkReady, setSdkReady] = useState(false)
+
   const dispatch = useDispatch()
 
   const userLogin = useSelector(state => state.userLogin)
 
   const bookingDetails = useSelector(state => state.bookingDetails)
   const { booking, loading, error } = bookingDetails
+
+  const bookingPay = useSelector(state => state.bookingPay)
+  const { loading: loadingPay, success: successPay } = bookingPay
 
   const addDecimals = num => {
     return (Math.round(num * 100) / 100).toFixed(2)
@@ -35,8 +36,34 @@ const BookingScreen = ({ match }) => {
   }
 
   useEffect(() => {
-    dispatch(getBookingDetails(bookingId))
-  }, [dispatch, bookingId])
+    const addPayPalScript = async () => {
+      const { data: clientId } = await axios.get('/api/config/paypal')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+      script.async = true
+      script.onload = () => {
+        setSdkReady(true)
+      }
+      document.body.appendChild(script)
+    }
+
+    if (!booking || successPay) {
+      dispatch({ type: BOOKING_PAY_RESET })
+      dispatch(getBookingDetails(bookingId))
+    } else if (!booking.isPaid) {
+      if (!window.paypal) {
+        addPayPalScript()
+      } else {
+        setSdkReady(true)
+      }
+    }
+  }, [dispatch, bookingId, successPay, booking])
+
+  const successPaymentHandler = paymentResult => {
+    console.log(paymentResult)
+    dispatch(payBooking(bookingId, paymentResult))
+  }
 
   return loading ? (
     <Loader />
@@ -79,7 +106,7 @@ const BookingScreen = ({ match }) => {
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <h2>Booking Items</h2>
+                <h2>Booking Events</h2>
                 {booking.bookingItems.length === 0 ? (
                   <Message>Booking is emapty</Message>
                 ) : (
@@ -130,6 +157,19 @@ const BookingScreen = ({ match }) => {
                     <Col>${booking.totalPrice}</Col>
                   </Row>
                 </ListGroup.Item>
+                {!booking.isPaid && (
+                  <ListGroup.Item>
+                    {loadingPay && <Loader />}
+                    {!sdkReady ? (
+                      <Loader />
+                    ) : (
+                      <PayPalButton
+                        amount={booking.totalPrice}
+                        onSuccess={successPaymentHandler}
+                      />
+                    )}
+                  </ListGroup.Item>
+                )}
               </ListGroup>
             </Card>
           </Col>
