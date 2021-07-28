4bc66bf9c579b3b442924d192f943616ac380ab0
PhilipNirushan <PhilipNirushan25@gmail.com>
2021-07-28 09:44:50 +0700

    Carousel Added

--- frontend/src/Components/ImageSlider.js
+++ frontend/src/Components/ImageSlider.js
@@ -1,48 +1,54 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap'
+import '../index.css'
+import { Carousel, Image } from 'react-bootstrap'
 
 const ImageSlider = () => {
-      
-    return (
-            <Carousel controls={false} fade={true}>
-                <Carousel.Item interval={2000}>
-                    <img
-                        className="d-block w-100"
-                        src="/Images/Carousel/img1.jpg"
-                        alt="First slide"
-                        height="600"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={2000}>
-                    <img
-                        className="d-block w-100"
-                        src="Images/Carousel/img2.jpg"
-                        alt="Second slide"
-                        height="600"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={2000}>
-                    <img
-                        className="d-block w-100"
-                        src="/Images/Carousel/img3.jpg"
-                        alt="Third slide"
-                        height="600"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-    )
+  return (
+    <Carousel controls={true} fade={true}>
+      <Carousel.Item interval={2000}>
+        <Image
+          className='d-block w-100'
+          src='/Images/Carousel/img1.jpg'
+          alt='First slide'
+          fluid
+        />
+        <Carousel.Caption className='carousel-text'>
+          <h3>First slide </h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item interval={2000}>
+        <Image
+          className='d-block w-100'
+          src='Images/Carousel/img2.jpg'
+          alt='Second slide'
+          fluid
+        />
+
+        <Carousel.Caption className='carousel-text'>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item interval={2000}>
+        <Image
+          className='d-block w-100'
+          src='/Images/Carousel/img3.jpg'
+          alt='Third slide'
+          fluid
+        />
+
+        <Carousel.Caption className='carousel-text'>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
 }
 
 export default ImageSlider
